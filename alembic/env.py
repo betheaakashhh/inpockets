99d559d0acbaf82153cc862cb338d18566287248
betheaakashhh <karmanam108@gmail.com>
2026-09-14 19:57:35 +0530

    alchemy

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,9 @@ from sqlalchemy import engine_from_config, pool
 
 from app.core.config import get_settings
 from app.db.base import Base
+from app.models.user import User
 
+print(Base.metadata.tables.keys())
 
 config = context.config
 
